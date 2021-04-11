b37d726cdbdf929ca84892cd22a517abc24ed61d
Anthony Castillo <anthonycastillo821@gmail.com>
2021-04-11 03:56:19 -0500

    Skeleton added to tests and lib files

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,38 +1,48 @@
-const Employee = require("../lib/employee");
+const Employee = require("../lib/Employee");
 
-describe('employee', () => {
+describe('Employee', () => {
 
     // Name
     it('should return a name when I pass a name to employee', () =>{
 
-        //Arrange
-        let employeeName = "Anthony";
-        //Act
-        let newEmployee = new Employee (employeeName);
+        // Arranging and Acting
+        let newEmployee = new Employee("Anthony", "096171", "acastcard@gmail.com")
         //Assert
-        expect(newEmployee.name).toBe("Anthony")
+        expect(newEmployee.empName).toBe("Anthony")
+
     });
 
     // id
+    it('should return an ID when I pass an ID to employee', () =>{
+
+        //Arrange/Acting
+        let newEmployee = new Employee ("Anthony", "096171", "acastcard@gmail.com");
+        //Assert
+        expect(newEmployee.id).toBe("096171")
+
+    });
     
     // email
+
+    it('should return an email when I pass an email to employee', () =>{
+
+        //Arrange/Acting
+        let newEmployee = new Employee ("Anthony", "096171", "acastcard@gmail.com");
+        //Assert
+        expect(newEmployee.email).toBe("acastcard@gmail.com")
+
+    });
     
+
+
     // getName()
+    
     
     // getId()
     
     // getEmail()
     
     // getRole()—returns 'Employee'
-
-
-
-
-
-
-
-
-
 
 
 
