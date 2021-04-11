ee6d9930fa0f0800e04b3b7b1d75fa5cad32001c
Anthony Castillo <anthonycastillo821@gmail.com>
2021-04-11 15:37:17 -0500

    Adding tests for Manager, fixed failing tests

---
+++ test/manager.test.js
@@ -0,0 +1,88 @@
+const Manager = require("../lib/Manager");
+
+describe('Manager', () => {
+
+    // Name
+    it('should return a name when I pass a name to Manager', () =>{
+
+        // Arranging and Acting
+        let newManager = new Manager("Christina", "213128", "cvvin@gmail.com", "7134")
+        //Assert
+        expect(newManager.empName).toBe("Christina")
+
+    });
+
+    // id
+    it('should return an ID when I pass an ID to Manager', () =>{
+
+        //Arrange/Acting
+        let newManager = new Manager ("Christina", "213128", "cvvin@gmail.com", "7134");
+        //Assert
+        expect(newManager.id).toBe("213128")
+
+    });
+    
+    // email
+    it('should return an email when I pass an email to Manager', () =>{
+
+        //Arrange/Acting
+        let newManager = new Manager ("Christina", "213128", "cvvin@gmail.com", "7134");
+        //Assert
+        expect(newManager.email).toBe("cvvin@gmail.com")
+
+    });
+    
+    // school
+    it('should return an office number when I pass an office number to Manager', () =>{
+
+        //Arrange/Acting
+        let newManager = new Manager ("Christina", "213128", "cvvin@gmail.com", "7134");
+        //Assert
+        expect(newManager.office).toBe("7134")
+
+    });
+    
+    // getName()
+    describe("getName", () => {
+       
+        it("should return the name of the Manager", () => {
+
+          expect(new Manager("Christina", "213128", "cvvin@gmail.com", "7134").getName()).toBe("Christina");
+        });
+       
+
+    });
+    
+    // getId()
+    describe("getId", () => {
+       
+        it("should get the Id of the Manager", () => {
+            
+          expect(new Manager("Christina", "213128", "cvvin@gmail.com", "7134").getId()).toBe("213128");
+        });
+       
+
+    });
+    
+    // getEmail()
+    describe("getEmail", () => {
+       
+        it("should get the email of the Manager", () => {
+            
+          expect(new Manager ("Christina", "213128", "cvvin@gmail.com", "7134").getEmail()).toBe("cvvin@gmail.com");
+        });
+       
+
+    });
+    
+    // getRole()—returns 'Manager'
+    describe("getRole", () => {
+       
+        it("should return the employee role as Manager", () => {
+            
+          expect(new Manager ("Christina", "213128", "cvvin@gmail.com", "7134").getRole()).toBe("Manager");
+        });
+       
+    });
+    
+})
